cb7132d3a0421bd90febe015868a61776a741b63
Frank <guozhi824@163.com>
2023-08-17 09:40:20 +0000

    implement http.Handler interface

--- gee-web/day1-http-base/base2/main.go
+++ gee-web/day1-http-base/base2/main.go
@@ -10,10 +10,20 @@ import (
 type Engine struct{}
 
 
-func (eg *Engine) ServeHTTP(w http.ResponseWritter, req *http.Request) {
+func (eg *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	switch req.URL.Path {
         case "/":
-		fmt.Fprintf(w, "URL.Path = %q\n", requ.URL.Path)
-	case
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	case "/hello":
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
 	}
+}
+
+func main(){
+	engine := new(Engine)
+	log.Fatal(http.ListenAndServe(":9999", engine))
 }
